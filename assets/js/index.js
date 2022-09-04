ce4cd57ad110153777e7d46a6eddea8687e579cc
smartgeneration <bay.nvit@gmail.com>
2022-09-04 16:52:17 +0700

    ver 1.2

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+//Get the button
+var mybutton = document.getElementById("to-top-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
